3fcbbadb99a61736bc6c9ee439c61263fbda1502
nmandery <nico.mandery@geops.de>
2010-07-01 19:24:23 +0200

    join classes and unittest

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension, Command
+from unittest import TextTestRunner, TestLoader
+
+class TestCommand(Command):
+  user_options = []
+    
+  def initialize_options(self):
+    pass
+
+  def finalize_options(self):
+    pass
+
+  def run(self):
+    import tests
+    loader = TestLoader()
+    t = TextTestRunner()
+    t.run(loader.loadTestsFromModule(tests))
+
 
 setup(name="pg_routejoin",
   version="0.1",
-  packages=['pg_routejoin'])
+  packages=['pg_routejoin'],
+  cmdclass = {"test": TestCommand})
+

77a73f383ea820ed5fcc25db43ebf1355772b076
nmandery <nico.mandery@geops.de>
2012-06-15 09:09:43 +0200

    do not expect strings witch comnmaseperated values as array as pg 9.x supports lists

--- routejoin/postgres.py
+++ routejoin/postgres.py
@@ -5,4 +5,6 @@ def sanitize_pg_array(pg_array):
     convert a array-string to a python list
     """
     # only for one-dimesional arrays
+    if not type(pg_array) in (str,unicode):
+        return pg_array
     return map(str.strip, pg_array.strip("{}").split(","))

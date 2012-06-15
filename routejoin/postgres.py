152b93c88d076b8ca4c242c918dca6d2c64474a8
nmandery <nico.mandery@geops.de>
2012-06-15 09:36:37 +0200

    commenting list conversion

--- routejoin/postgres.py
+++ routejoin/postgres.py
@@ -3,8 +3,12 @@
 def sanitize_pg_array(pg_array):
     """
     convert a array-string to a python list
+
+    PG <9.0 used comma-aperated strings as array datatype. this function
+    will convert those to list. if pg_array is not a tring, it will not
+    be modified
     """
-    # only for one-dimesional arrays
     if not type(pg_array) in (str,unicode):
         return pg_array
+    # only for one-dimesional arrays
     return map(str.strip, pg_array.strip("{}").split(","))
